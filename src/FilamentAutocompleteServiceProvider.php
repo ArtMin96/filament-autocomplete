d69883562f389fd8e5442b6836b51929623c74de
Arthur Minasyan <ArtMin96@users.noreply.github.com>
2023-11-22 15:17:41 +0400

    wip

--- src/FilamentAutocompleteServiceProvider.php
+++ src/FilamentAutocompleteServiceProvider.php
@@ -2,39 +2,15 @@
 
 namespace ArtMin96\FilamentAutocomplete;
 
-use Filament\PluginServiceProvider;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Css;
 use Filament\Support\Facades\FilamentAsset;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentAutocompleteServiceProvider extends PluginServiceProvider
+class FilamentAutocompleteServiceProvider extends PackageServiceProvider
 {
     public static string $name = 'filament-autocomplete';
-
-    protected array $resources = [
-        // CustomResource::class,
-    ];
-
-    protected array $pages = [
-        // CustomPage::class,
-    ];
-
-    protected array $widgets = [
-        // CustomWidget::class,
-    ];
-
-    protected array $styles = [
-        'plugin-filament-autocomplete' => __DIR__ . '/../resources/dist/filament-autocomplete.css',
-    ];
-
-    protected array $scripts = [
-        'plugin-filament-autocomplete' => __DIR__ . '/../resources/dist/filament-autocomplete.js',
-    ];
-
-    // protected array $beforeCoreScripts = [
-    //     'plugin-filament-autocomplete' => __DIR__ . '/../resources/dist/filament-autocomplete.js',
-    // ];
 
     public function configurePackage(Package $package): void
     {
@@ -42,7 +18,7 @@ class FilamentAutocompleteServiceProvider extends PluginServiceProvider
             ->hasViews();
     }
 
-    public function packageBooted(): void
+    public function packageRegistered(): void
     {
         FilamentAsset::register([
             Css::make('filament-autocomplete', __DIR__ . '/../dist/css/filament-autocomplete.css')->loadedOnRequest(),
